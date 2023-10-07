a1eaae38a2a0061039f9d923ae1eabd1cce7ffe2
Akash NP <akashnp1998@gmail.com>
2023-10-07 09:59:43 +0400

    feat: serializer author info, send back username
    ensures API consumer can display author name

--- django_simple_comments/serializers.py
+++ django_simple_comments/serializers.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
 
 from .models import Comment
 
 
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+
 class CommentSerializer(ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Comment
         fields = [
